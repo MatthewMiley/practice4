26fd7d85743e98c13243901c9a61524171476788
Ana Cabrera-Bello <acabrerabello9810@floridapoly.edu>
2025-11-12 22:38:33 +0000

    blending the two parts together

--- game.py
+++ game.py
@@ -27,24 +27,47 @@ def play_game():
     verified = verify_input(user_input)
 
     # if input is valid
-    if verified = True:
-        print(f"You chose: {user_input}")
+    if verified == True:
+        print(f"You chose: {user_input.title()}")
 
     # if input is invalid, exit the function
     else:
-        print(f"{user_input} is not valid")
+        print(f"{user_input} is not a valid choice")
         return
     
-    # generate a random number from 0 to 2
-    print("Welcome to Rock-Paper-Scissors!")
-
+    # computer picks
     computer_choices = ['Rock', 'Paper', 'Scissors']
 
     computer_input = random.choice(computer_choices)
 
     print(f"Computer chose: {computer_input}")
 
+
     # see if user won or lost
+    if user_input.title() == 'Rock':
+        if computer_input == 'Rock':
+            print('You tied')
+        elif computer_input == 'Paper':
+            print('You lost')
+        elif computer_input == 'Scissors':
+            print('You won')
+
+    elif user_input.title() == 'Paper':
+        if computer_input == 'Rock':
+            print('You won')
+        elif computer_input == 'Paper':
+            print('You tied')
+        elif computer_input == 'Scissors':
+            print('You lost')
     
+    elif user_input.title() == 'Scissors':
+        if computer_input == 'Rock':
+            print('You lost')
+        elif computer_input == 'Paper':
+            print('You won')
+        elif computer_input == 'Scissors':
+            print('You tied')
+    
+    return
 
-
+play_game()
